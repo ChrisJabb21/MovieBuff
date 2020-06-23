9a8a7699b7d1d372ec54b4fa283d75538117d994
ChrisJabb21 <chrisjabbour94@gmail.com>
2020-06-23 16:02:00 -0400

    Added directory and used creat-react-app template

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import SearchMovies from "./SearchMovies";
+import 'public/css/style.css';
+import 'public/css/normalize.css';
+
+//import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+ <div className="container">
+        <h1 className="title">React Movie Search</h1>
+    <SearchMovies/>
+</div>
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -15,3 +22,21 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+////////project from scrimba index.js
+/*
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SearchMovies from "./SearchMovies";
+
+class Main extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <h1 className="title">React Movie Search</h1>
+        <SearchMovies/>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Main />, document.getElementById('root')); */
