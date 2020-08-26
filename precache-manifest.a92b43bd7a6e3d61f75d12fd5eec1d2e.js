d520473fcd1c5aa5c4c2d93b9ded4bfe27a2fb85
ChrisJabb21 <chrisjabbour94@gmail.com>
2020-08-26 14:07:17 -0400

    Updates

---
+++ precache-manifest.a92b43bd7a6e3d61f75d12fd5eec1d2e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "821ac3bc7b48c2fcee0a115e69431907",
+    "url": "/MovieBuff/index.html"
+  },
+  {
+    "revision": "be23e8375674fa482e2e",
+    "url": "/MovieBuff/static/css/main.f20db94b.chunk.css"
+  },
+  {
+    "revision": "8b261404557c110ec5b0",
+    "url": "/MovieBuff/static/js/2.312d2922.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/MovieBuff/static/js/2.312d2922.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be23e8375674fa482e2e",
+    "url": "/MovieBuff/static/js/main.3c550c3c.chunk.js"
+  },
+  {
+    "revision": "dcadb83436f8bd133753",
+    "url": "/MovieBuff/static/js/runtime-main.30193210.js"
+  },
+  {
+    "revision": "ee2a0d5336596cca4cd5ce13f25eb016",
+    "url": "/MovieBuff/static/media/arnold.ee2a0d53.jpg"
+  },
+  {
+    "revision": "a9d340c3275af7a14909c1a27a74ce3d",
+    "url": "/MovieBuff/static/media/retrowave.a9d340c3.jpg"
+  }
+]);
